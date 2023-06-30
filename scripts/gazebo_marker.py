a340d888971758db9b4435d1a7e2f516002ef93f
Chen Chen <maker_cc@foxmail.com>
2023-06-30 12:38:59 +0800

    add color for goal and a pid controller

--- scripts/gazebo_marker.py
+++ scripts/gazebo_marker.py
@@ -5,6 +5,7 @@ Automatically publish goal marker in gazebo, rviz and tf server
 author: Chen Chen
 """
 
+from __future__ import annotations
 import os
 
 import rospy
@@ -15,40 +16,83 @@ from visualization_msgs.msg import Marker
 
 
 class GazeboMarker:
-    def __init__(self, pub, x, y, z, scale=0.1, id=2) -> None:
+    def __init__(
+        self,
+        pub: rospy.Publisher,
+        x: float,
+        y: float,
+        z: float,
+        scale: float = 0.1,
+        id: int = 2,
+        frame: str = "odom",
+        color: tuple[float, float, float, float] = (1.0, 1.0, 1.0, 0.8),
+    ) -> None:
         self.pub = pub
         self.x = x
         self.y = y
         self.z = z
         self.scale = scale
         self.id = id
-        # gazebo marker
-        msg = f"action: ADD_MODIFY, type: SPHERE, id: {self.id}, "
-        msg += f"scale: {{x:{self.scale}, y:{self.scale}, z:{self.scale}}}, "
-        msg += f"pose: {{position: {{x:{self.x}, y:{self.y}, z:{self.z}}}, orientation: {{x:0, y:0, z:0, w:1}}}}"
-        os.system("gz marker -m '" + msg + "'")
-        # rviz marker
-        marker = Marker(
-            type=Marker.SPHERE,
-            id=self.id,
-            pose=Pose(Point(self.x, self.y, self.z), Quaternion(0, 0, 0, 1)),
-            scale=Vector3(*(self.scale,) * 3),
-            header=Header(frame_id="odom"),
-            color=ColorRGBA(1.0, 1.0, 1.0, 0.8),
-        )
-        self.pub.publish(marker)
-        # tf broadcast
+        self.frame = frame
+        self.color = color
         self.br = tf2_ros.StaticTransformBroadcaster()
+        self.publish_marker()
+
+    def publish_marker(self):
+        self.publish_gazebo_marker()
+        self.publish_rviz_marker()
+        self.broadcast_tf()
+
+    def broadcast_tf(self):
         tf_stamped = TransformStamped(
-            header=Header(stamp=rospy.Time.now(), frame_id="odom"),
-            child_frame_id="ee_goal",
+            header=Header(stamp=rospy.Time.now(), frame_id=self.frame),
+            child_frame_id=f"ee_goal{self.id}",
             transform=Transform(
                 translation=Vector3(self.x, self.y, self.z), rotation=Quaternion(0, 0, 0, 1)
             ),
         )
         self.br.sendTransform(tf_stamped)
 
+    def publish_rviz_marker(self):
+        marker = Marker(
+            type=Marker.SPHERE,
+            id=self.id,
+            pose=Pose(Point(self.x, self.y, self.z), Quaternion(0, 0, 0, 1)),
+            scale=Vector3(*(self.scale,) * 3),
+            header=Header(frame_id=self.frame),
+            color=ColorRGBA(*self.color),
+        )
+        self.pub.publish(marker)
+
+    def publish_gazebo_marker(self):
+        msg = f"action: ADD_MODIFY, type: SPHERE, id: {self.id}, "
+        msg += f"scale: {{x:{self.scale}, y:{self.scale}, z:{self.scale}}}, "
+        msg += f"pose: {{position: {{x:{self.x}, y:{self.y}, z:{self.z}}}, orientation: {{x:0, y:0, z:0, w:1}}}}"
+        os.system("gz marker -m '" + msg + "'")
+
+    @property
+    def position(self) -> tuple[float, float, float]:
+        return (self.x, self.y, self.z)
+
+    @position.setter
+    def position(self, value: tuple[float, float, float]):
+        if len(value) != 3:
+            raise AttributeError("position is not 3 dimentional")
+        self.x, self.y, self.z = value
+
+    def update(
+        self,
+        pos: tuple[float, float, float] | None = None,
+        color: tuple[float, float, float, float] | None = None,
+    ):
+        if pos is not None:
+            self.position = pos
+        if color is not None:
+            self.color = color
+        self.publish_marker()
+
     def __del__(self) -> None:
+        # cannot delete tf frame
         os.system(f"gz marker -m 'action: DELETE_MARKER, id: {self.id}'")
         marker = Marker(action=Marker.DELETE, id=self.id)
         self.pub.publish(marker)
@@ -62,6 +106,8 @@ if __name__ == "__main__":
     rospy.on_shutdown(marker.__del__)
 
     rate = rospy.Rate(1)
+
+    marker.update(pos=(0, 1, 2), color=(0, 1, 0, 0.8))
 
     try:
         while not rospy.is_shutdown():
