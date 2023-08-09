5ca1480644e8c9126cb2a594b33bab6a57c744b1
Chen Chen <maker_cc@foxmail.com>
2023-08-08 17:02:23 -0700

    add gripper into joystick interface also add autorepeat and joy selector

--- da_interface/scripts/joy_interface.py
+++ da_interface/scripts/joy_interface.py
@@ -4,22 +4,26 @@ from __future__ import annotations
 
 import rospy
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64, Float64MultiArray
 
 
 class JoyconInterface:
     def __init__(self) -> None:
         rospy.Subscriber("/joy", Joy, self.interface_cb)
         self.publisher = rospy.Publisher("teleop_velocity_command", Float64MultiArray, queue_size=1)
-        self.axis_assign = [3, 2, 1, 0, 4]  # base trans, base turn, lift, arm, wrist
-        self.axis_factors = [0.25, 0.75, 1, -0.25, 3]
+        self.gripper_publisher = rospy.Publisher(
+            "/stretch_controller/gripper_cmd", Float64, queue_size=1
+        )
+        self.axis_assign = [1, 0, 4, 3, 6, 7]  # base trans, base turn, lift, arm, wrist, gripper
+        self.axis_factors = [0.25, 0.75, 0.3, -0.05, 0.5, 1]
 
     def interface_cb(self, msg: Joy) -> None:
-        vel = [0] * 5
+        vel = [0] * 6
         print(msg.axes)
-        for idx in range(5):
+        for idx in range(6):
             vel[idx] = msg.axes[self.axis_assign[idx]] * self.axis_factors[idx]
-        self.publisher.publish(Float64MultiArray(data=[*vel[:3], *[vel[3]] * 4, vel[4], 0]))
+        self.publisher.publish(Float64MultiArray(data=[*vel[:3], *[vel[3]] * 4, vel[4]]))
+        self.gripper_publisher.publish(Float64(data=vel[5]))
 
 
 if __name__ == "__main__":
